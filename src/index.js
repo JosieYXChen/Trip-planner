0b77e749c411d09dec6b3a20e1e10729c4cdfa4d
Kevin Zieber <zieber.kevinr@gmail.com>
2020-10-05 11:03:11 -0500

    got our map working!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('This is running!');
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = "pk.eyJ1Ijoia2V2aW56aWViZXIiLCJhIjoiY2tmd3BzNXgxMW05czJxcWc3ZWppa2dsbyJ9.gfog29iwH22Jqlq5545hrg";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+});
