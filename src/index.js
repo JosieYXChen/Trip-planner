ce20f2d0993cce51f48e920b4e733140c8be847f
Josie Chen <josieyxchen@gmail.com>
2020-10-05 13:45:39 -0400

    Map

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import createMarker from "./marker";
 
 mapboxgl.accessToken = "pk.eyJ1Ijoia2V2aW56aWViZXIiLCJhIjoiY2tmd3BzNXgxMW05czJxcWc3ZWppa2dsbyJ9.gfog29iwH22Jqlq5545hrg";
 
-const map = new mapboxgl.Map({
+export const map = new mapboxgl.Map({
   container: "map",
   center: [-87.6354, 41.8885],
   zoom: 12,
